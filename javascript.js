c5946c90ded52900fbd035d83176ebd0e0a12eed
Anonymous_PC <anonymous_pc_serbia@protonmail.com>
2022-07-25 12:26:33 -0400

    Formatted some things, corrected typo in Logo.
    Formatted some things, corrected typo in Logo.

--- javascript.js
+++ javascript.js
@@ -1,18 +1,17 @@
-function mouseOver(e){
+function mouseOver(e) {
 
     console.log(e);
 
-   this.classList.toggle('squareOn');
+    this.classList.toggle('squareOn');
 }
 
-    const container = document.querySelector('#container');
+const container = document.querySelector('#container');
 
-    for(let i = 0; i < 256; i++)
-    {
-        let square = document.createElement('div'); 
-        square.classList.add('square'); 
-        square.addEventListener("mouseover", mouseOver)
-        square.style.text
-        container.appendChild(square);
-    }
+for (let i = 0; i < 256; i++) {
+    let square = document.createElement('div');
+    square.classList.add('square');
+    square.addEventListener("mouseover", mouseOver)
+    square.style.text
+    container.appendChild(square);
+}
 
