754ed219802fcb0126e9b9a45361b9dbe23d653f
Anonymous_PC <anonymous_pc_serbia@protonmail.com>
2022-07-25 12:50:55 -0400

    Got clear button working...
    Got clear button working...

--- javascript.js
+++ javascript.js
@@ -1,12 +1,11 @@
-function mouseOver(e) {
-
-    console.log(e);
-
-    this.classList.toggle('squareOn');
-}
-
+/*Create 16x16 Grid=============================================================*/
 const container = document.querySelector('#container');
 
+function mouseOver(e) {
+    console.log(e);
+    this.classList.toggle('squareOn');
+
+}
 for (let i = 0; i < 256; i++) {
     let square = document.createElement('div');
     square.classList.add('square');
@@ -15,3 +14,10 @@ for (let i = 0; i < 256; i++) {
     container.appendChild(square);
 }
 
+/*Clear Button=============================================================*/
+function clear() {
+    const squares = document.querySelectorAll('.squareOn');
+    squares.forEach(square => square.classList.toggle('squareOn'));
+}
+const clearButton = document.querySelector('#clearButton');
+clearButton.addEventListener("mousedown", clear);
