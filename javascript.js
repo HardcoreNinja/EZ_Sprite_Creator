ba0e5c60baea9d61f82239ee9b10e648ee2404dd
Anonymous_PC <anonymous_pc_serbia@protonmail.com>
2022-07-25 12:24:05 -0400

    Created nav bar, added custom font, cleaned up some things.
    Created nav bar, added custom font, cleaned up some things.

--- javascript.js
+++ javascript.js
@@ -1,22 +1,17 @@
-function mouseDown(e){
+function mouseOver(e){
 
     console.log(e);
-
 
    this.classList.toggle('squareOn');
 }
 
-function mouseOut(e){
-    console.log(e);
-    this.classList.remove('squareOn');
-}
     const container = document.querySelector('#container');
 
     for(let i = 0; i < 256; i++)
     {
         let square = document.createElement('div'); 
         square.classList.add('square'); 
-        square.addEventListener("mousedown", mouseDown)
+        square.addEventListener("mouseover", mouseOver)
         square.style.text
         container.appendChild(square);
     }
