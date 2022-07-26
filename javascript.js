4913881ddb28f6ab476cc56b1937bf1375e32238
Anonymous_PC <anonymous_pc_serbia@protonmail.com>
2022-07-25 21:31:12 -0400

    Got color chooser working.
    Got color chooser working.

--- javascript.js
+++ javascript.js
@@ -1,23 +1,48 @@
-/*Create 16x16 Grid=============================================================*/
-const container = document.querySelector('#container');
+/*Variables======================================================================*/
+let color = 'black';
 
-function mouseOver(e) {
-    console.log(e);
-    this.classList.toggle('squareOn');
 
-}
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement('div');
-    square.classList.add('square');
-    square.addEventListener("mouseover", mouseOver)
-    square.style.text
-    container.appendChild(square);
-}
-
-/*Clear Button=============================================================*/
+/*Clear Button=================================================================*/
 function clear() {
-    const squares = document.querySelectorAll('.squareOn');
-    squares.forEach(square => square.classList.toggle('squareOn'));
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.style.backgroundColor = "transparent");
 }
+
 const clearButton = document.querySelector('#clearButton');
 clearButton.addEventListener("mousedown", clear);
+
+/*Create 16x16 Grid=============================================================*/
+function mouseOver() {
+    if (this.style.backgroundColor === "transparent")
+        this.style.backgroundColor = color;
+    else if (this.style.backgroundColor !== "transparent")
+        this.style.backgroundColor = "transparent";
+}
+
+function drawGrid() {
+    const container = document.querySelector('#container');
+
+    for (let i = 0; i < 256; i++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        square.addEventListener("mouseover", mouseOver)
+        container.appendChild(square);
+    }
+    
+    clear();
+}
+
+window.onload = drawGrid();
+
+
+/*Color Picker==================================================================*/
+function changeColor(e) {
+    color = e.target.value;
+}
+
+const colorPicker = document.querySelector('#colorPicker');
+colorPicker.addEventListener('change', changeColor);
+
+
+
+
