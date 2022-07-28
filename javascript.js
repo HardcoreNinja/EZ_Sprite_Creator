6d1fb3ffd66705083228350e075aa4b2e502ad46
Anonymous_PC <anonymous_pc_serbia@protonmail.com>
2022-07-28 10:14:23 -0400

    Got preview modal to display properly.
    Got preview modal to display properly.

--- javascript.js
+++ javascript.js
@@ -71,6 +71,33 @@ function hideGrid() {
 
 hideGridButton.addEventListener('mousedown', hideGrid);
 
+/*Preview Modal========================================================================*/
+// Get the modal
+var modal = document.getElementById("previewModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("downloadButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 /*Paint Color Picker==================================================================*/
 function changeColor(e) {
     //console.log("Paint Color: " +  e.target.value);
