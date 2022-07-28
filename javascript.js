bce52c0fa7e84006265709167267c1700b3e9d0d
Anonymous_PC <anonymous_pc_serbia@protonmail.com>
2022-07-27 20:36:24 -0400

    Implemented Hide Grid Button.
    Implemented Hide Grid Button.

--- javascript.js
+++ javascript.js
@@ -4,11 +4,12 @@ let containerSize = 480;
 let squaresPerRow = 16;
 let numSquares = Math.pow(squaresPerRow, 2);
 let squarePixelSize = (containerSize / squaresPerRow) - 2;
+let hideGridBool = false;
 
 /*Clear Button=================================================================*/
 function clear() {
     const squares = document.querySelectorAll('.square');
-    squares.forEach(function(square){
+    squares.forEach(function (square) {
         square.style.backgroundColor = "transparent";
         square.style.borderColor = "white";
     });
@@ -19,13 +20,11 @@ clearButton.addEventListener("mousedown", clear);
 
 /*Create 16x16 Grid=============================================================*/
 function mouseDown() {
-    if (this.style.backgroundColor === "transparent")
-    {
+    if (this.style.backgroundColor === "transparent") {
         this.style.backgroundColor = paintColor;
         this.style.borderColor = paintColor;
     }
-    else if (this.style.backgroundColor !== "transparent")
-    {
+    else if (this.style.backgroundColor !== "transparent") {
         this.style.backgroundColor = "transparent";
         this.style.borderColor = "white";
     }
@@ -48,6 +47,29 @@ function drawGrid() {
 
 window.onload = drawGrid();
 
+/*Hide Grid Button=====================================================================*/
+const hideGridButton = document.querySelector('#hideGridButton');
+
+function hideGrid() {
+    hideGridBool = !hideGridBool
+    const squares = document.querySelectorAll('.square');
+    
+    if (hideGridBool) {
+        squares.forEach(function (square) {
+            if (square.style.backgroundColor === 'transparent')
+                square.style.borderColor = 'transparent';
+        });
+    }
+    else if(!hideGridBool)
+    {
+        squares.forEach(function (square) {
+            if (square.style.backgroundColor === 'transparent')
+                square.style.borderColor = 'white';
+        });
+    }
+}
+
+hideGridButton.addEventListener('mousedown', hideGrid);
 
 /*Paint Color Picker==================================================================*/
 function changeColor(e) {
@@ -59,15 +81,15 @@ const colorPicker = document.querySelector('#paintColorPicker');
 colorPicker.addEventListener('input', changeColor);
 
 /*Slider========================================================================*/
-function adjustSize(e){
+function adjustSize(e) {
     //console.log(e.target.value);
     const container = document.querySelector('#container');
 
     while (container.firstChild) {
         container.removeChild(container.firstChild);
-}
-    containerSize = 480;    
-    squaresPerRow = e.target.value; 
+    }
+    containerSize = 480;
+    squaresPerRow = e.target.value;
     numSquares = Math.pow(squaresPerRow, 2);
     squarePixelSize = (containerSize / squaresPerRow) - 2;
 
